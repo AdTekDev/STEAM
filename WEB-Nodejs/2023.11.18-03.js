5571132f9829e9f1f9853a320d39876101fc7148
AdTekDev <nguyenngtu777@gmail.com>
2023-11-18 16:57:59 +0700

    Create 2023.11.18-03.js

---
+++ WEB-Nodejs/2023.11.18-03.js
@@ -0,0 +1,33 @@
+express = require("express")
+app = express()
+PORT = 80
+//-----------------------------------------------------
+
+app.get("/", (req, res) => {
+    res.send("day la trang home !")
+})
+
+app.get("/gioithieu", (req, res) => {
+    res.send("day la trang Gioi Thieu !")
+})
+
+app.get("/login", (req, res) => {
+    res.send("day la trang login !")
+})
+
+app.get("/logout", (req, res) => {
+    res.send("day la trang logout !")
+})
+
+app.get("/sanpham", (req, res) => {
+    res.send("day la trang San Pham !")
+})
+
+app.get("/thanhtoan", (req, res) => {
+    res.send("<h1 style='color:red'>day la trang Thanh Toan !</h1>")
+})
+
+//-----------------------------------------------------
+app.listen(PORT, () => {
+    console.log(`WEB da mo tai CONG ${PORT}`)
+})
