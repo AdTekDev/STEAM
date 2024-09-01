0797542f1bd145ff84a8d7771b97a22a2dec8181
AdTekDev <nguyenngtu777@gmail.com>
2024-09-01 07:45:08 +0700

    Add files via upload

--- DB-MySQL/index.js
+++ DB-MySQL/index.js
@@ -17,17 +17,46 @@ connectionSQL.connect(function(err) {
 
 
 app.get('/', (req, res) => {
-  //res.send('Hello World!')
+        res.send('University - Home Page!')
+})
+
+app.get('/giangvien', (req, res) => {
   connectionSQL.query("SELECT * FROM giangvien", function (err, result, fields) {
     if (err) 
         {
             res.send('Hello World!');
             throw err;
         }
-        res.send(result);
+        //res.send(result);
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text/html');
+        
+        console.log(typeof(result), result.length, result[0].magv);
+        for (var i = 0; i < result.length; i++){
+            res.write(
+                "<hr> <div>"
+                + " <p> Ma so GV: " + result[i].magv + "</p>"
+                + " <p> Ho ten GV: " + result[i].hotengv + "</p>"
+                + " <p> Khoa: " + result[i].makhoa + "</p>"
+                + "</div>"
+            );
+        }
+        res.end();
   });
 
 })
+
+app.get('/load/:table', (req, res) => {
+    //res.send('Hello World!')
+    connectionSQL.query("SELECT * FROM " + req.params.table, function (err, result, fields) {
+      if (err) 
+          {
+              res.send('tables !');
+              throw err;
+          }
+          res.send(result);
+    });
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
